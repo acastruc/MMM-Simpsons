d0262d81b28a2d7300660c518af96d666d520652
Adam Castrucci <5711953+acastruc@users.noreply.github.com>
2024-02-17 23:20:39 -0500

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,37 +1,45 @@
-const fetch = require("node-fetch");
 var NodeHelper = require("node_helper");
-var cheerio = require("cheerio");
+const Log = require("logger");
 
 module.exports = NodeHelper.create({
 	
 	start: function() {
-		console.log("Starting node helper: " + this.name);
+		Log.Info("Starting node helper: " + this.name);
 	},
 	
 	socketNotificationReceived: function(notification, payload) {
 		var self = this;
-		console.log("SimpsonsQuote -> Notification: " + notification + " Payload: " + payload);
+		Log.Info("SimpsonsQuote -> Notification: " + notification + " Payload: " + payload);
 		
 		if(notification === "GET_SIMPSONSQUOTE") {
 			
 			var url = "https://thesimpsonsquoteapi.glitch.me/quotes";
 			
-			console.log('-> SimpsonsQuote request');
-
-			fetch(url)
-				.then(response => response.text())
-				.then(body => {
-					console.log("body=" + body)
-					var $ = cheerio.load(body);
-					console.log("$=" + $)
+			Log.Info('-> SimpsonsQuote request');
+			fetch(uri).then((response) => response.json())
+  				  .then(data => {
+      					Log.Info("Simpsons Quote : " + data)
 					//var src = $(".img-comic").attr('src');
 					//console.log('Simpsons Quote img -> ' + src);
-					self.sendSocketNotification("SIMPSONSQUOTE", { data : body });
-				})
-				.catch((error) => {
-					console.log('Simpsons Quote Fetch Error -> ' + error);
-				});
-			return;
+					//self.sendSocketNotification("SIMPSONSQUOTE", { data : body });
+				  })
+				  .catch(error) {
+				      Log.Error("Failed to fetch quote:" + error);
+				  }
+			//fetch(url)
+			//	.then(response => response.text())
+			//	.then(body => {
+			//		console.log("body=" + body)
+			//		var $ = cheerio.load(body);
+			//		console.log("$=" + $)
+			//		//var src = $(".img-comic").attr('src');
+			//		//console.log('Simpsons Quote img -> ' + src);
+			//		self.sendSocketNotification("SIMPSONSQUOTE", { data : body });
+			//	})
+			//	.catch((error) => {
+			//		console.log('Simpsons Quote Fetch Error -> ' + error);
+			//	});
+			//return;
 			// request(url, function (error, response, body) {
 			// 	console.log('SimpsonsQuote -> ' + body);
 			// 	self.sendSocketNotification("SIMPSONSQUOTE", { data : body });
